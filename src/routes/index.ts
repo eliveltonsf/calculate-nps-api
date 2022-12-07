238accd38e058a775d04a3a4c4147767888e267a
Elivelton Ferreira <elivelton.seven@gmail.com>
2022-12-07 14:19:37 -0300

    :hammer: control instance

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,6 @@ import { UserController } from '../controllers/UserController';
 
 const routes = Router();
 
-const userController = new UserController();
-
-routes.post('/users', userController.create);
+routes.post('/users', new UserController().create);
 
 export default routes;
