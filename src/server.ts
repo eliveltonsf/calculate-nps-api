2ec6f1c9c617418e2d98fd51ea72c26a06fab3a9
Elivelton Ferreira <elivelton.seven@gmail.com>
2022-12-07 10:35:41 -0300

    :sparkles: add usercontroller

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import { AppDataSource } from './data-source';
 import express from 'express';
+import routes from './routes';
 
 AppDataSource.initialize().then(() => {
   const app = express();
+
+  app.use(express.json());
+
+  app.use(routes);
 
   app.listen(process.env.PORT, () => console.log('Server is running!'));
 });
