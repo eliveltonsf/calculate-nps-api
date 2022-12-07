330bb985684f6f87a14332d64f89563247b574f5
Elivelton Ferreira <elivelton.seven@gmail.com>
2022-12-07 16:44:57 -0300

    :sparkles: creating registration and listing routes for surveys

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import SurveysController from '../controllers/SurveysController';
 import { UserController } from '../controllers/UserController';
 
 const routes = Router();
 
 routes.post('/users', new UserController().create);
+
+routes.post('/surveys', new SurveysController().create);
+routes.get('/surveys', new SurveysController().show);
 
 export default routes;
