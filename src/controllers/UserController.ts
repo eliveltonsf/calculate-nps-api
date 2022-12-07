d360b1328ca8ecd99461ef66dc7c2151213a67be
Elivelton Ferreira <elivelton.seven@gmail.com>
2022-12-07 14:23:45 -0300

    :sparkles: adding rules and sending data to the database

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,37 @@
+import { userRepository } from './../repositories/UserRepository';
 import { Request, Response } from 'express';
 
 class UserController {
   async create(request: Request, response: Response) {
-    const body = request.body;
+    const { name, email } = request.body;
 
-    return response.send();
+    const userAlreadyExits = await userRepository.findOneBy({
+      email,
+    });
+
+    if (userAlreadyExits) {
+      return response.status(400).json({ message: 'User already exists!' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ message: 'O nome é obrigatório!' });
+    }
+    if (!email) {
+      return response.status(400).json({ message: 'O email é obrigatório!' });
+    }
+
+    try {
+      const newUser = userRepository.create({
+        name,
+        email,
+      });
+
+      await userRepository.save(newUser);
+
+      return response.status(201).json(newUser);
+    } catch (error) {
+      return response.status(500).json({ message: 'Internal Server Error!', error });
+    }
   }
 }
 
