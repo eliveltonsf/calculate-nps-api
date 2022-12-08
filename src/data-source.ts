86a3c6eee45e359d5f6c7f262f907c891fdd6181
Elivelton Ferreira <elivelton.seven@gmail.com>
2022-12-08 10:26:01 -0300

    :sparkles: preparing environment for testing with jest

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
-  database: './src/database/database.sqlite',
+  database: process.env.NODE_ENV === 'test' ? './src/database/database.test.sqlite' : './src/database/database.sqlite',
   entities: [`${__dirname}/**/models/*.{ts,js}`],
   migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 });
