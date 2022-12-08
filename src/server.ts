e2329492da05b659774079dc76d8e27c07d16302
Elivelton Ferreira <elivelton.seven@gmail.com>
2022-12-08 10:23:29 -0300

    :hammer: refactoring server configuration files

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
+import app from './app';
 import { AppDataSource } from './data-source';
-import express from 'express';
-import routes from './routes';
 
 AppDataSource.initialize().then(() => {
-  const app = express();
-
-  app.use(express.json());
-
-  app.use(routes);
-
   app.listen(process.env.PORT, () => console.log('Server is running!'));
 });
