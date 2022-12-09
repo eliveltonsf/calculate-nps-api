e3fd98e1fd2a666a301015838c2cbcdf5e8ccfac
Elivelton Ferreira <elivelton.seven@gmail.com>
2022-12-09 11:14:28 -0300

    :sparkles: using environment variables and adding rules for sending email

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,20 +3,18 @@ import surveysRepository from '../repositories/SurveysRepository';
 import surveysUsersRepository from '../repositories/SurveysUsersRepository';
 import userRepository from '../repositories/UserRepository';
 import SendMailService from '../services/SendMailService';
+import { resolve } from 'path';
+import 'dotenv';
 
 class SendMailController {
   async execute(request: Request, response: Response) {
     const { email, surveyId } = request.body;
 
-    console.log({ surveyId });
-
-    const userAlreadyExits = await userRepository.findOneBy({
+    const dataUser = await userRepository.findOneBy({
       email,
     });
 
-    console.log(userAlreadyExits);
-
-    if (!userAlreadyExits) {
+    if (!dataUser) {
       return response.status(400).json({
         error: 'User does not exists!',
       });
@@ -24,21 +22,38 @@ class SendMailController {
 
     const dataSurvey = await surveysRepository.findOneBy({ id: surveyId });
 
-    console.log(dataSurvey);
-
     if (!dataSurvey) {
       return response.status(400).json({
         error: 'Survey does not exists!',
       });
     }
 
+    const variables = {
+      name: dataUser.name,
+      title: dataSurvey.title,
+      description: dataSurvey.description,
+      user_id: dataUser.id,
+      link: process.env.URL_MAIL,
+    };
+
+    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
+
+    const dataSurveyUsersExists = await surveysUsersRepository.findOneBy({ user: { id: dataUser.id } });
+    const dataUsersSurveyExists = await surveysUsersRepository.findOneBy({ survey: { id: dataSurvey.id } });
+
+    if (dataSurveyUsersExists && dataUsersSurveyExists) {
+      await SendMailService.execute(email, dataSurvey.title, variables, npsPath);
+      return response.json(dataUsersSurveyExists);
+    }
+
     const surveyUser = surveysUsersRepository.create({
-      user: userAlreadyExits.id,
+      user: dataUser.id,
       survey: surveyId,
     });
+
     await surveysUsersRepository.save(surveyUser);
 
-    await SendMailService.execute(email, dataSurvey.title, dataSurvey.description);
+    await SendMailService.execute(email, dataSurvey.title, variables, npsPath);
 
     return response.json(surveyUser);
   }
